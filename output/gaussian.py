324f47d49cfed2ce33ca3992eed1413b291be266
Ryan <ryan@chezmojo.com>
2016-05-08 08:04:26 +0200

    Default four times ncpus gb of memory for Gaussian

--- output/gaussian.py
+++ output/gaussian.py
@@ -7,7 +7,7 @@ class Writer(xyz.Writer):
     def _writehead(self,fh):
         fh.write('%%chk=%s.chk\n' % self.jobname)
         fh.write('%%nprocshared=%s\n' % self.opts.ncpus)
-        fh.write('%%mem=%sGB\n' % str(self.opts.ncpus*2) )
+        fh.write('%%mem=%sGB\n' % str(self.opts.ncpus*4) )
         fh.write('# b3lyp/lanl2dz GFPrint\n\n')
         fh.write('%s\n\n' % self.jobname)
         fh.write(' 0 %s\n' % xyz.Writer.getMultiplicity(self.parser.zmat) )
@@ -18,7 +18,7 @@ class Writer(xyz.Writer):
             fh.write('--Link1--\n')
             fh.write('%%chk=%s.chk\n' % self.jobname)
             fh.write('%%nprocshared=%s\n' % self.opts.ncpus)
-            fh.write('%%mem=%sGB\n' % str(self.opts.ncpus*2) )
+            fh.write('%%mem=%sGB\n' % str(self.opts.ncpus*4) )
             fh.write('# b3lyp/lanl2dz guess=read\n')
             fh.write('# iop(5/33=3)\n')
             fh.write('# iop(3/33=1)\n\n')
