4aa3227448413a2e74d03760850557d4b5c18a04
Ryan <ryan@chezmojo.com>
2016-05-09 12:20:26 +0200

    LAN2DZ ECP for Orca with -T

--- output/orca.py
+++ output/orca.py
@@ -9,7 +9,10 @@ class Writer(xyz.Writer):
         self.logger.info('Writing to %s' % fn)
         mult = xyz.Writer.getMultiplicity(self.parser.zmat)
         with open(fn,'wt') as fh:
-            fh.write('! DFT B3LYP Def2-TZVP ECP{def2-TZVP}\n') 
+            if self.opts.transport:
+                fh.write('! DFT B3LYP Def2-TZVP ECP{LANL2,LANLDZ}\n') 
+            else:
+                fh.write('! DFT B3LYP Def2-TZVP ECP{def2-TZVP}\n') 
             fh.write('#! AHSCF vdwgrid3\n') 
             fh.write('#%method SFitInvertType Diag_Q end\n')
             fh.write('* xyzfile 0 %s %s\n'% (mult,xyzfh.name) )
@@ -17,7 +20,8 @@ class Writer(xyz.Writer):
             if not self.opts.transport:
                return
             fh.write('\n$new_job\n')
-            fh.write('! DFT B3LYP Def2-TZVP ECP{def2-TZVP} MOREAD\n')
+            #fh.write('! DFT B3LYP Def2-TZVP ECP{def2-TZVP} MOREAD\n')
+            fh.write('! DFT B3LYP Def2-TZVP ECP{LANL2,LANLDZ} MOREAD\n') 
             fh.write('#! AHSCF vdwgrid3\n') 
             fh.write('#%method SFitInvertType Diag_Q end\n')
             fh.write('* xyzfile 0 %s %s\n' % (mult,xyzfh.name) )
