32dec5dfc08fb0dae4ab03cacc761397fa00d611
Ryan <ryan@chezmojo.com>
2016-05-09 07:45:38 +0200

    Added bigger grid and more robust SCF to orca.

--- output/orca.py
+++ output/orca.py
@@ -10,12 +10,14 @@ class Writer(xyz.Writer):
         mult = xyz.Writer.getMultiplicity(self.parser.zmat)
         with open(fn,'wt') as fh:
             fh.write('! DFT B3LYP Def2-TZVP ECP{def2-TZVP}\n') 
+            fh.write('#! AHSCF vdwgrid3\n') 
             fh.write('* xyzfile 0 %s %s\n'% (mult,xyzfh.name) )
             fh.write('%pal nprocs 48 end\n')
             if not self.opts.transport:
                return
             fh.write('\n$new_job\n')
             fh.write('! DFT B3LYP Def2-TZVP ECP{def2-TZVP} MOREAD\n')
+            fh.write('#! AHSCF vdwgrid3\n') 
             fh.write('* xyzfile 0 %s %s\n' % (mult,xyzfh.name) )
             fh.write('%%MoInp "%s.gbw"\n' % bn)
             fh.write('%%base "%s_T"\n' % bn)
