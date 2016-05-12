cc020bfdd2fbfa4f178df5d17a3843bf718affac
Ryan <ryan@chezmojo.com>
2016-05-12 13:57:36 +0200

    Typeo in gollum out

--- output/gollum.py
+++ output/gollum.py
@@ -43,7 +43,7 @@ class Writer(xyz.Writer):
          
     def _writezmat(self,fh):
         #e = self._guesselectrodes()
-        #m = {"L":"1 1 1","M":"0 0 0","R":"2 1 2"}
+        m = {"L":"1 1 1","M":"0 0 0","R":"2 1 2"}
         if not self.parser.haselectrodes():
             self.logger.error("Error finding electrodes!")
             fh.write("Error in QuantumParse!")
