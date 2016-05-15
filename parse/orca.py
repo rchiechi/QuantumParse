5536e8a5bd3fa73903156c2978601dd329a4bba8
Ryan Chiechi <ryan@chezmojo.com>
2016-05-15 16:31:07 +0200

    Started cleanup of orcamatrix parsing

--- parse/orca.py
+++ parse/orca.py
@@ -2,6 +2,7 @@ from parse import Parser
 
 import sys,os,re
 import numpy as np
+from collections import OrderedDict
 
 from parse import xyz
 import logging
@@ -36,47 +37,36 @@ class Parser(xyz.Parser):
                 self.zmat.buildElectrodes(self.opts.build)
             self.zmat.findElectrodes()
 
+logger = logging.getLogger('OrcaMatrix')
+
 def overlap(fh):
     print("Parsing overlap matrix...")
     fh.seek(0)
-    ovdict = {}
+    ovdict = OrderedDict()
     inoverlap = False
     lk = []
-    rp = re.compile('\s')
     for l in fh:
         lk.append(l.strip())
         if not inoverlap:
-            if len(lk) > 3:
-                lk.pop(0)
-            else:
-                continue
+            if len(lk) > 3: lk.pop(0)
+            else: continue
         if lk[1] == 'OVERLAP MATRIX' and not inoverlap:
             inoverlap = True
             continue
-        elif l[0] == '-' and inoverlap:
-            inoverlap = False
-            break
+        elif l[0] == '-' and inoverlap: break
         if inoverlap:
-            lsf = filter(None, re.split(rp,l))
+            lsf = l.split()
             fl = []
             for n in lsf:
-                if '.' in n:
-                    fl.append(float(n))
-                else:
-                    fl.append(int(n))
+                if '.' in n: fl.append(float(n))
+                else: fl.append(int(n))
             if isinstance(fl[-1],float):
-                i = fl[0]
-                if i not in ovdict:
-                    ovdict[i] = fl[1:]
-                else:
-                    ovdict[i] += fl[1:]
+                if fl[0] not in ovdict: ovdict[fl[0]] = fl[1:]
+                else: ovdict[fl[0]] += fl[1:]
     print("%sOverlap Matrix " % Fore.YELLOW, end='')
     print("%sx-elements: %s%s, %sy-elements: %s%s%s" \
             % (Fore.YELLOW,Fore.GREEN,len(ovdict),Fore.YELLOW,Fore.GREEN,len(ovdict[0]),Style.RESET_ALL) )
-    ovmatrix = []
-    for i in range(0, len(ovdict)):
-        ovmatrix.append(ovdict[i])
-    return np.array(ovmatrix,np.float)
+    return np.array(list(ovdict.values()),np.float)
 
 def fock(fh):
     fh.seek(0)
@@ -89,9 +79,8 @@ def fock(fh):
     sys.stdout.flush()
     for l in fh:
         l = l.strip()
-        if key in l:
-            scfidx += 1
-    print(scfidx)
+        if key in l: scfidx += 1
+    print(Fore.GREEN+scfidx+Style.RESET_ALL)
     fh.seek(0)
     iscf = 0 
     print("Parsing Fock matrix...",end='')
@@ -99,7 +88,8 @@ def fock(fh):
     for l in fh:
         l = l.strip()
         if 'ERROR' in l:
-            print(l)
+            logger.error('Orca calculation did not converge!')
+            sys.exit()
         if key in l:
             iscf += 1
             if not iscf % 5:
@@ -126,11 +116,10 @@ def fock(fh):
                 if '.' in n:
                     try:
                         n = float(n)
-                        if n == 0:
-                            n = abs(n)
+                        if n == 0: n = abs(n)
                         fl.append(n)
                     except ValueError as msg:
-                        print(msg)
+                        logger.warning('Error parsing fock matrix: %s' %str(msg))
                         ditch=True
                         continue
                 else:
@@ -141,7 +130,7 @@ def fock(fh):
             if isinstance(fl[-1],float) and isinstance(fl[0],int):
                 norb.append(fl[0])
                 if 0 < norb[-1] <= norb[-2]:
-                    #print("Out-of-order orbital: %s <= %s" % (norb[-1],norb[-2]))
+                    logger.warning("Out-of-order orbital: %s <= %s" % (norb[-1],norb[-2]))
                     continue
                 i = fl[0]
                 if i not in fockdict:
@@ -152,14 +141,13 @@ def fock(fh):
     fockmatrix = []
     for i in sorted(fockdict.keys()):
         if len(fockdict[i]) != len(fockdict):
-            print("Matrix alignment error: {%s} " % i, end='')
+            logger.error("Matrix alignment error: {%s} " % i, end='')
             sys.exit()
         else:
             fockmatrix.append(fockdict[i])
     fm = np.array(fockmatrix, np.float)
     if 0 in fm.shape:
-        print("Empty Fock Matrix! ",end='')
-        print(fm.shape)
+        logger.error("Empty Fock Matrix! Shape: %s" % str(fm.shape) )
         sys.exit()
     print("%sFock matrix " % Fore.YELLOW,end='')
     print("%sx-elements: %s%s, %sy-elements: %s%s%s" \
@@ -168,13 +156,13 @@ def fock(fh):
 
 def norbs(fh):
     fh.seek(0)
-    orbdict = {}
+    orbdict = OrderedDict()
     inorb = False
     orbidx = []
     lk = []
     rp = re.compile('^\d+\D+$')
     lidx = 0
-    print("Parsing molecular orbitals...",end='')
+    logger.info("Parsing molecular orbitals...",end='')
     for l in fh:
         lidx += 1
         if not lidx%1000000:
@@ -182,37 +170,25 @@ def norbs(fh):
             sys.stdout.flush()
         if not inorb:
             lk.append(l.strip())
-            if len(lk) > 3:
-                lk.pop(0)
-            else:
-                continue
-        if lk[1] == 'MOLECULAR ORBITALS' and not inorb: 
-            inorb = True
-        elif l[0] == '*' and inorb:
-            inorb = False
-            break
+            if len(lk) > 3: lk.pop(0)
+            else: continue
+        if lk[1] == 'MOLECULAR ORBITALS' and not inorb: inorb = True
+        elif l[0] == '*' and inorb: break
         if inorb:
-            lsf = []
-            for ln in l.split():
-                lsf.append(ln)
-            if not len(lsf):
-                continue
-            elif re.match(rp,lsf[0]) == None:
-                continue
+            lsf = l.split()
+            if not lsf: continue
+            elif re.match(rp,lsf[0]) == None: continue
             if lsf[0] in orbdict:
-                if lsf[1] in orbdict[lsf[0]]:
-                    continue
-                else:
-                    orbdict[lsf[0]].append(lsf[1])
+                if lsf[1] in orbdict[lsf[0]]: continue
+                else: orbdict[lsf[0]].append(lsf[1])
             else:
                 orbdict[lsf[0]] = [lsf[1]]
                 orbidx.append(lsf[0])
-    sys.stdout.write("\n")
+    print('.')
     torbs = 0
-    for a in orbdict:
-        torbs+=len(orbdict[a])
+    for a in orbdict: torbs+=len(orbdict[a])
     if torbs == 0:
-        print("%sNo orbitals found, caclulation probably did not converge!%s" % (Fore.RED,Style.RESET_ALL))
+        logger.error("No orbitals found, caclulation probably did not converge!%s")
         sys.exit()
     else:
         print("%sAtoms: %s%s %sOrbitals: %s%s%s" \
