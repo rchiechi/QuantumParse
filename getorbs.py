b671419af40bf60942f3a6c8ce13d50f5fa851a3
Ryan <ryan@chezmojo.com>
2016-06-09 08:47:42 +0200

    Use noiter in orca

--- getorbs.py
+++ getorbs.py
@@ -9,7 +9,7 @@ hn = os.uname()[1]
 if 'pg' in hn or 'peregrine' in hn:
     ORCABIN=''
 else:
-    ORCABIN=os.path.join(os.environ['HOME'],'orca','bin')
+    ORCABIN=os.path.join(os.environ['HOME'],'Library','Orca','bin')
 #print("Orca bin dir is %s" % ORCABIN)
 
 def GetOrbsOrca(fn):
--- output/orca.py
+++ output/orca.py
@@ -25,8 +25,7 @@ class Writer(xyz.Writer):
                return
             fh.write('\n$new_job\n')
             #fh.write('! DFT B3LYP Def2-TZVP ECP{LANL2,LANLDZ} vdwgrid3 MOREAD\n') 
-            fh.write('! DFT B3LYP/G DUNNING-DZP ECP{LANL2,LANLDZ} vdwgrid3 MOREAD\n') 
-            fh.write('%scf MaxIter 1 end\n') 
+            fh.write('! DFT B3LYP/G DUNNING-DZP ECP{LANL2,LANLDZ} vdwgrid3 NOITER MOREAD\n') 
             fh.write('#! AHSCF\n') 
             fh.write('#%method SFitInvertType Diag_Q end\n')
             fh.write('* xyzfile 0 %s %s\n' % (mult,xyzfh.name) )
