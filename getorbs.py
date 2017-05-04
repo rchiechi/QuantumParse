c2f30b21d1eb5563794e354e1f74641cdf2eeb83
Ryan <ryan@chezmojo.com>
2017-05-04 07:11:12 +0200

    Color and isovalue selection

--- getorbs.py
+++ getorbs.py
@@ -2,28 +2,8 @@
 
 import sys,os,re,argparse,subprocess
 
-hn = os.uname()[1]
-OSNAME=os.uname()[0]
-if OSNAME == 'Darwin':
-    p = subprocess.run(['find', '/Applications', '-maxdepth', '3', '-type', 'd', '-name', 'VMD*app'],stdout=subprocess.PIPE)
-    VMDBIN=p.stdout.strip().split(b'\n')[-1]
 
-elif OSNAME == 'Linux':
-    p = subprocess.run(['which','vmd'],stdout=subprocess.PIPE)
-    VMDBIN=p.stdout.strip()
-else:
-    VMDBIN=None
-
-ORCABIN=None
-for b in ('orca', 'orca.sh'):
-    p = subprocess.run(['which', b],stdout=subprocess.PIPE)
-    if p.returncode == 0:
-        ORCABIN=p.stdout.strip()
-
-if VMDBIN:
-    print("Found VMD: %s" % VMDBIN)
-if ORCABIN:
-    print("Found Orca: %s" % ORCABIN)
+# # # # # # # # # # # # # # # # FUNCTIONS  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 def GetOrbsOrca(fn):
     orbs = []
@@ -115,24 +95,63 @@ def writeVMD(fn):
             if m == 0: i = 2
             else: i = 0
             fh.write('mol addrep %s\n' % m) 
-            fh.write('mol modstyle %s %s Isosurface 0.003 0 0 0\n' % (i,m))
-            fh.write('mol modcolor %s %s ColorID 0\n' % (i,m))
+            fh.write('mol modstyle %s %s Isosurface %s 0 0 0\n' % (i,m,opts.isovalue))
+            fh.write('mol modcolor %s %s ColorID %s\n' % (i,m,VMDCOLORS[opts.colors[0]]))
             fh.write('mol modmaterial %s %s Translucent\n' % (i,m))
             fh.write('mol addrep %s\n' % m) 
-            fh.write('mol modstyle %s %s Isosurface -0.003 0 0 0\n' % (i+1,m))
-            fh.write('mol modcolor %s %s ColorID 1\n' % (i+1,m))
+            fh.write('mol modstyle %s %s Isosurface -%s 0 0 0\n' % (i+1,m,opts.isovalue))
+            fh.write('mol modcolor %s %s ColorID %s\n' % (i+1,m,VMDCOLORS[opts.colors[1]]))
             fh.write('mol modmaterial %s %s Translucent\n' % (i+1,m))
         if mols > 0:
             for m in range(1,mols+1):
                 fh.write('mol delrep 2 %s\n' % m)
                 fh.write('mol showrep %s %s off\n' % (m,0))
                 fh.write('mol showrep %s %s off\n' % (m,1))
+        fh.write('menu graphics on\n')
         #fh.write('render Wavefront /Volumes/Data/rchiechi/Calculations/orca/AQ/aqhomo.obj false\n')
     print('Wrote %s' % fn)
 
-# # # # # # # # # # # # 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-parser = argparse.ArgumentParser(description='Find orbitals in Orca and NWChem outputs'\
+
+
+
+
+VMDCOLORS={'blue':0,'red':1,'gray':2,'orange':3,'yellow':4,
+        'tan':5,'silver':6,'green':7,'white':8,'pink':9,
+        'cyan':10,'purple':11,'lime':12,'mauve':13,
+        'ochre':14,'iceblue':15,'black':16,'yellow2':17,
+        'yellow3':18,'green2':19,'green3':20,'cyan2':21,
+        'cyan3':22,'blue2':23,'blue3':24,'violet':25,
+        'violet2':26,'magenta':27,'magenta2':28,
+        'red2':29,'red3':30,'orange2':31,'orange3':32}
+      
+hn = os.uname()[1]
+OSNAME=os.uname()[0]
+if OSNAME == 'Darwin':
+    p = subprocess.run(['find', '/Applications', '-maxdepth', '3', '-type', 'd', '-name', 'VMD*app'],stdout=subprocess.PIPE)
+    VMDBIN=os.path.join(p.stdout.strip().split(b'\n')[-1],b'Contents/MacOS/startup.command')
+
+elif OSNAME == 'Linux':
+    p = subprocess.run(['which','vmd'],stdout=subprocess.PIPE)
+    VMDBIN=p.stdout.strip()
+else:
+    VMDBIN=None
+
+ORCABIN=None
+for b in ('orca', 'orca.sh'):
+    p = subprocess.run(['which', b],stdout=subprocess.PIPE)
+    if p.returncode == 0:
+        ORCABIN=p.stdout.strip()
+
+if VMDBIN:
+    print("Found VMD: %s" % VMDBIN)
+if ORCABIN:
+    print("Found Orca: %s" % ORCABIN)
+
+
+
+parser = argparse.ArgumentParser(description='Find orbitals in Orca outputs'\
         ,formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('infiles', type=str, nargs='*', default=[], 
@@ -141,8 +160,12 @@ parser.add_argument('-o', '--orbs', type=str, nargs='*', default=['HOMO','LUMO']
     help='Orbitals to render (e.g., HOMO-1, LUMO+1.')
 parser.add_argument('-r','--render', action='store_true',  default=False, 
     help='If the appropriate programs are found, render the orbitals automatically.')
-parser.add_argument('-g','--gbw', type=str,  default='', 
+parser.add_argument('-g','--gbw', type=str,  default='guess', 
     help='Manually specify a GBW file instead of guessing from output file.')
+parser.add_argument('-c','--colors', nargs=2, default=['blue','red'],  choices=tuple(VMDCOLORS.keys()), 
+    help='Colors for +/- orbital coefficients.')
+parser.add_argument('-i','--isovalue', type=float, default=0.005, 
+    help='Cutoff for isoplots.')
 
 opts=parser.parse_args()
 
@@ -150,9 +173,13 @@ if not opts.infiles:
     print("No input file.")
     sys.exit()
 
+if len(opts.colors) != 2:
+    print("Invalid color selection %s" % str(opts.colors))
+    sys.exit()
+
+
 for fn in opts.infiles:
     ORBS={}
-        
     with open(fn, 'rt') as fh:
         h = fh.read(2048)
         if 'nwchem' in h.lower():
@@ -164,12 +191,10 @@ for fn in opts.infiles:
             continue
     print("# # # # # # # # %s (%s) # # # # # # # #" % (fn,PROG))
     BN = os.path.basename(fn)[:-4]
-    HOMO,LUMO=0,0
-    heng,leng=0,0
     if PROG=='orca':
         try:
             orbs = GetOrbsOrca(fn)
-            if opts.gbw:
+            if opts.gbw != 'guess':
                 gbw = opts.gbw
             else:
                 gbw = FindGBW(fn)
@@ -199,6 +224,7 @@ for fn in opts.infiles:
         for o in ORBS:
             print('%s: (%0.4f eV)' % (o,ORBS[o][1]))
         print('# # # # # # # # # # # # # # # # # # # # # # # #')
+        RUNORCA=False
         fn = '%s_plot.inp' % BN
         with open(fn, 'wt') as fh:
             fh.write('! DFT B3LYP/G LANL2DZ MOREAD NOITER\n')
@@ -206,6 +232,7 @@ for fn in opts.infiles:
             fh.write('* xyzfile 0 1 %s.xyz\n' % gbw[:-4])
             fh.write('%%base "%s-plot"\n' % BN)
             fh.write('%%MoInp "%s"\n' % gbw)
+            #fh.write('%pal nprocs 8 end\n') 
             fh.write('%plots\n')
             fh.write('dim1  128   # resolution in x-direction\n')
             fh.write('dim2  128   # resolution in y-direction\n')
@@ -213,52 +240,60 @@ for fn in opts.infiles:
             fh.write('Format Gaussian_Cube\n')
             for o in ORBS:
                 fh.write('MO("%s.cube",%s,0);  # orbital to plot\n' % (ORBS[o][2],ORBS[o][0]))
+                if not os.path.exists("%s.cube" % ORBS[o][2]):
+                    RUNORCA=True
             fh.write('end\n')
         print("Write %s" % fn)
         tclfn = fn[:-4]+'_vmd.tcl'
         writeVMD(tclfn)
         orcasuccess=False
-        if ORCABIN and opts.render:
+        if ORCABIN and RUNORCA and opts.render:
             print('Starting orca...')
             p = subprocess.run([ORCABIN,fn],stdout=subprocess.PIPE)
             if b'****ORCA TERMINATED NORMALLY****' in p.stdout:
                 print('Successfully generated cube files with Orca.')
-                orcasuccess=True
+                orcasuccess = True
             else:
-                print('Something may have gone wrong with Orca, check the output.')
-                print('\n'.join(p.stdout.strip().split(b'\n')))
-        if VMDBIN and orcasuccess:
-            if OSNAME == 'Darwin':
-                print("I am going to launch VMD now, but you probably have to copy/paste this command into the terminal:")
-                print("source %s" % os.path.join(os.getcwd(),tclfn))
-                subprocess.run(['open',VMDBIN, '--args', '-e', tclfn])
-            else:
-                subprocess.run([VMDBIN, '-e', tclfn])
-    elif PROG == 'nwchem':
-        try:
-            orbs = GetOrbsNwchem(fn)
-        except FileNotFoundError:
-            print("%s does not exist." % fn)
-            continue
-        for i in range(0, len(orbs)):
-            #Vector 1363  Occ=0.000000D+00  E= 2.383287D+01  Symmetry=bu
-            orbnum = int(orbs[i][1])
-            orbocc = float(orbs[i][2].split('=')[1].replace('D','E'))
-            if orbocc == 0:
-                HOMO = int(orbs[i-1][1])
-                LUMO = orbnum
-                heng = float(orbs[i-1][3].split('=')[1].replace('D','E'))
-                leng = float(orbs[i][3].split('=')[1].replace('D','E'))
-                break
-        print("# # # %s_orbs.nw # # #" % BN)
-        dplot_homo, dplot_lumo = writedplot(BN, HOMO,heng,LUMO,leng)
-        with open(BN+'_orbs.nw', 'wt') as fh:
-            fh.write('restart %s\n' % BN)
-            fh.write('title %s_orbs\n' % BN)
-            fh.write('memory 1 GB\n\n\n')
-            fh.write(dplot_homo)
-            fh.write('task dplot\n\n')
-            fh.write(dplot_lumo)
-            fh.write('task dplot\n\n')
+                print('Something may have gone wrong with Orca, check the output:')
+                #stdout = p.stdout.strip().split(b'\n')
+                #for i in range(len(stdout)-10,len(stdout)):
+                #    print(stdout[i])
+                subprocess.run(['tail','-n','15',fn[:-4]+'.out'])
+        elif ORCABIN and opts.render:
+            orcasuccess = True
+            print("Skipping Orca run because cube files already exist.")
+            print("* * * * * * * * * * *\n")
 
-        print('HOMO: %s (%0.4f eV), LUMO: %s (%0.4f eV)' % (HOMO,heng*27.2107,LUMO,leng*27.2107))
+        if VMDBIN and orcasuccess:
+            subprocess.run([VMDBIN, '-e', tclfn])
+
+#    elif PROG == 'nwchem':
+#       HOMO,LUMO=0,0
+#       heng,leng=0,0
+#
+#        try:
+#            orbs = GetOrbsNwchem(fn)
+#        except FileNotFoundError:
+#            print("%s does not exist." % fn)
+#            continue
+#        for i in range(0, len(orbs)):
+#            #Vector 1363  Occ=0.000000D+00  E= 2.383287D+01  Symmetry=bu
+#            orbnum = int(orbs[i][1])
+#            orbocc = float(orbs[i][2].split('=')[1].replace('D','E'))
+#            if orbocc == 0:
+#                HOMO = int(orbs[i-1][1])
+#                LUMO = orbnum
+#                heng = float(orbs[i-1][3].split('=')[1].replace('D','E'))
+#                leng = float(orbs[i][3].split('=')[1].replace('D','E'))
+#                break
+#        print("# # # %s_orbs.nw # # #" % BN)
+#        dplot_homo, dplot_lumo = writedplot(BN, HOMO,heng,LUMO,leng)
+#        with open(BN+'_orbs.nw', 'wt') as fh:
+#            fh.write('restart %s\n' % BN)
+#            fh.write('title %s_orbs\n' % BN)
+#            fh.write('memory 1 GB\n\n\n')
+#            fh.write(dplot_homo)
+#            fh.write('task dplot\n\n')
+#            fh.write(dplot_lumo)
+#            fh.write('task dplot\n\n')
+#        print('HOMO: %s (%0.4f eV), LUMO: %s (%0.4f eV)' % (HOMO,heng*27.2107,LUMO,leng*27.2107))
