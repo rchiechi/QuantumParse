8aacdcdb97306d161e033bb57c9e583d97a25ed0
Ryan <ryan@chezmojo.com>
2017-05-11 13:53:14 +0200

    Add argparse

--- MakeSAM.py
+++ MakeSAM.py
@@ -4,40 +4,46 @@
 # THIS IS A TEST: DO NOT USE
 #
 
-import sys,os
+import sys,os,argparse
 from ase.io import read,write
 from ase.build import fcc111,add_adsorbate
 
-height=1.5
-position='hcp'
-size=[10,10,2]
 
-slab=fcc111('Au',size)
-slab.info['adsorbate_info']['top layer atom index'] = len(b.positions)-1
-AC=read('AQ.xyz')
+
+# Parse command line arguments
+desc='Build a SAM from an XYZ file containing a molecule projected along the Z-axis'
+parser = argparse.ArgumentParser(description=desc,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('infile', type=str, nargs=1, default=[], 
+    help='XYZ file to parse.')
+parser.add_argument('-H', '--height', type=int, default=1.5, 
+    help='Height in Angstroms to place molecules above surface.')
+parser.add_argument('-e','--electrode', type=str, default='Au', 
+    help='Type of electrode.')
+parser.add_argument('-s','--size', type=str, default='10,10,2', 
+    help='Size of the substrate in the form x,y,z number of atoms.')
+
+
+opts=parser.parse_args()
+
+if not infile:
+    print("I need an input file.")
+    sys.exit()
+
+#height=1.5
+position='hcp'
+#size=[10,10,2]
+
+opts.size = opts.size.strip().split(',')
+
+slab=fcc111(opts.electrode,opts.size)
+slab.info['adsorbate_info']['top layer atom index'] = len(slab.positions)-1
+AC=read(opts.infile)
 print(AC)
 for i in range(1,size[0]-1,2):
     add_adsorbate(slab,AC,height,position,offset=[0,i],mol_index=0)
     for n in range(2,size[0]-1,2):
         add_adsorbate(slab,AC,height,position,offset=[n,i],mol_index=0)
-
-    
-
-write('test.png',b,rotation='80x,180z')
-write('test.xyz',b)
-def add_mol(slab,ads):
-    # Get the z-coordinate:
-    if 'top layer atom index' in info:
-        a = info['top layer atom index']
-    else:
-        a = slab.positions[:, 2].argmax()
-        if 'adsorbate_info' not in slab.info:
-            slab.info['adsorbate_info'] = {}
-        slab.info['adsorbate_info']['top layer atom index'] = a
-    z = slab.positions[a, 2] + height
-
-    # Move adsorbate into position
-    ads.translate([pos[0], pos[1], z] - ads.positions[mol_index])
-
-    # Attach the adsorbate
-    slab.extend(ads)
+write(opts.infile[:-4]+'.png',slab,rotation='80x,180z')
+write(opts.infile[:-4]+'_SAM.xyz',slab)
