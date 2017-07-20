8c16e31df05972f07a4508690a8283e97d16d2e5
Ryan <ryan@chezmojo.com>
2017-07-20 12:19:38 +0200

    Use cclib to parse Guassian logs

--- parse/gaussian.py
+++ parse/gaussian.py
@@ -1,4 +1,21 @@
 from parse import xyz
+import logging
+from util import *
+from ase import Atom,Atoms
+from cclib.io import ccread
 
 class Parser(xyz.Parser):
     breaks = ('--link1--','natoms=','stoichiometry')
+
+    def _parsezmat(self):
+        self.logger.debug('Building zmatrix...')
+        zmat = ZMatrix()
+        fh = ccread(self.fn)
+        for i in range(0, len(fh.atomnos)):
+            zmat += Atom(fh.atomnos[i], fh.atomcoords[-1][i])
+        if self.opts.project:
+            zmat.toZaxis()
+        elif self.opts.sortaxis:
+            zmat.sort(self.opts.sortaxis)
+        self.zmat = zmat
+        self.logger.info('Found: %s' % self.zmat.get_chemical_formula())
