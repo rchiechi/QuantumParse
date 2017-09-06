35673e5d1eeccc8b0ca9fe0942e4bcc33dd07092
Ryan <rchiechi@gmail.com>
2017-09-06 09:42:18 +0200

    writing without bonds is pointless

--- output/h5mol.py
+++ output/h5mol.py
@@ -1,4 +1,5 @@
 from output import xyz
+import numpy as np
 import h5py
 
 class Writer(xyz.Writer):
@@ -13,10 +14,18 @@ class Writer(xyz.Writer):
         coords = []
         masses = []
         for _atom in self.parser.zmat:
-            anums.append(_atom['number'])
-            coords.append(_atom['position'])
-            masses.append(_atom['mass'])
-         
+            anums.append(_atom.number)
+            coords.append(_atom.position)
+            masses.append(_atom.mass)
+        #TODO hackish
+        fn = fh.name
+        fh.close()
+        with h5py.File(fn, 'w') as fh:
+            fh.create_dataset('atomicNumbers',  data=np.array(anums) , dtype='i8')  
+            fh.create_dataset('coordinates',  data=np.array(coords) , dtype='f8')  
+            fh.create_dataset('masses',  data=np.array(masses) , dtype='f8')  
+            fh.create_dataset('title', data=np.array( bytes(self.jobname,encoding='utf8') ) )
+
         #<HDF5 dataset "atomicNumbers": shape (34,), type "<i8">
         #<HDF5 group "/bondOrders" (36 members)>
         #<HDF5 group "/bonds" (36 members)>
