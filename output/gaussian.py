1ef70f6be82f577fca12f53203c0a132cfa16532
Ryan Chiechi <rchiech@ncsu.edu>
2022-04-29 13:04:25 -0400

    Change default to Def2SVP

--- output/gaussian.py
+++ output/gaussian.py
@@ -1,16 +1,16 @@
 from output import xyz
 
 class Writer(xyz.Writer):
-    
+
     ext = '.com'
 
     def _writehead(self,fh):
         fh.write('%%chk=%s.chk\n' % self.jobname)
         fh.write('%%nprocshared=%s\n' % self.opts.ncpus)
-        fh.write('%%mem=%sGB\n' % str(self.opts.ncpus*4) )
-        fh.write('# b3lyp/lanl2dz GFPrint\n\n')
+        fh.write('%%mem=%sGB\n' % str(self.opts.ncpus*4))
+        fh.write('# b3lyp/Def2SVP GFPrint\n\n')
         fh.write('%s\n\n' % self.jobname)
-        fh.write(' 0 %s\n' % xyz.Writer.getMultiplicity(self.parser.zmat) )
+        fh.write(' 0 %s\n' % xyz.Writer.getMultiplicity(self.parser.zmat))
 
     def _writetail(self,fh):
         fh.write('\n')
@@ -18,12 +18,11 @@ class Writer(xyz.Writer):
             fh.write('--Link1--\n')
             fh.write('%%chk=%s.chk\n' % self.jobname)
             fh.write('%%nprocshared=%s\n' % self.opts.ncpus)
-            fh.write('%%mem=%sGB\n' % str(self.opts.ncpus*4) )
+            fh.write('%%mem=%sGB\n' % str(self.opts.ncpus*4))
             fh.write('# b3lyp/lanl2dz guess=read\n')
             fh.write('# iop(5/33=3)\n')
             fh.write('# iop(3/33=1)\n\n')
             fh.write('%s\n\n' % self.jobname)
-            fh.write(' 0 %s\n' % xyz.Writer.getMultiplicity(self.parser.zmat) )
+            fh.write(' 0 %s\n' % xyz.Writer.getMultiplicity(self.parser.zmat))
             self._writezmat(fh)
             fh.write('\n')
-
