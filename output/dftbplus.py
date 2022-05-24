3d6ef7431741fa11b96fcaba8438e24c4f627377
Ryan Chiechi <rchiech@ncsu.edu>
2022-05-24 15:26:00 -0400

    Forgot a )

--- output/dftbplus.py
+++ output/dftbplus.py
@@ -11,18 +11,20 @@ def get_angular_momentum(elem):
 
 class Writer(xyz.Writer):
     'Basic dftb_in.hsd file to do a geometry optimization'
+
     def _writetail(self, fh):
         fn = 'dftb_in.hsd'
         self.logger.info('Writing to %s', fn)
         with open(fn,'wt') as hsd:
             hsd.write('Geometry = xyzFormat {\n')
-            hsd.write(f'<<< {fh.name}.xyz\n}\n' )
+            hsd.write(f'<<< {fh.name}.xyz\n')
+            hsd.write('}\n')
             hsd.write('Driver = ConjugateGradient {\n')
             hsd.write(f'  MaxSteps = 10000\n  MovedAtoms = 1:-1\n  OutputPrefix = "{fh.name}_opt"\n')
             hsd.write('  #MaxForceComponent = 1.0e-4      # Stop if maximal force below 1.0e-4\n')
             hsd.write('Hamiltonian = DFTB {\n')
             hsd.write('  #ReadInitialCharges = Yes\n  SCCTolerance = 1E-7\n')
-            hsd.write('  Scc = Yes\n  MaxSccIterations = 100\n'
+            hsd.write('  Scc = Yes\n  MaxSccIterations = 100\n')
             hsd.write('  SlaterKosterFiles = Type2FileNames { # File names with two atom type names\n')
             hsd.write(f'    Prefix = "{os.path.expanduser("~")}/.dftbplus/slakos/3ob-3-1/"    # Prefix before first type name\n')
             hsd.write('    Separator = "-" # Dash between type names\n')
